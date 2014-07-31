3ac0bae2eaa9c0b4b79ba075a8058a472492a86d
Matthew Thompson <matt@mattathompson.org>
2014-07-31 01:16:08 -0400

    Letter_counter

---
+++ 7-30-14/letter_counter.rb
@@ -0,0 +1,25 @@
+class LetterCounter 
+
+
+  def initialize
+    @letter = "a"
+    @count_hash = {}
+  end
+
+  def count_meh
+    puts "Enter a string: "
+    user_input = gets.chomp.downcase
+
+    while @letter.length == 1
+      letter_add = user_input.count("#{@letter}")
+      @count_hash.store(@letter.to_s, letter_add)
+      @letter.succ!
+    end
+
+    puts @count_hash
+  end
+
+count = LetterCounter.new
+count.count_meh
+
+end
